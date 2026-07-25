87d315f592043987862be3a29377a9c99a4d275a
jhuma45683968-eng <jhuma45683968@gmail.com>
2026-07-25 19:30:44 +0600

    Create banners.js

---
+++ banners.js
@@ -0,0 +1,18 @@
+export const allVideosBanners = [
+    {
+        imageName: "IMG_20260725_141801_602.jpg", // ১ম ভিডিওর ছবির নাম
+        title: "ভিডিও টাইটেল ১",
+        telegramLink: "https://t.me/s2PIkZvtaT5lNmY1/3"
+    },
+    {
+        imageName: "another_image_2.jpg",           // ২য় ভিডিওর ছবির নাম
+        title: "ভিডিও টাইটেল ২",
+        telegramLink: "https://t.me/your_channel/2"
+    },
+    {
+        imageName: "another_image_3.jpg",           // ৩য় ভিডিওর ছবির নাম
+        title: "ভিডিও টাইটেল ৩",
+        telegramLink: "https://t.me/your_channel/3"
+    }
+    // এভাবে কমা দিয়ে নিচে নিচে আরও যত খুশি যোগ করতে পারবেন
+];
